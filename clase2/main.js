44c206a3910a4afb43eee083909edfb3831cadec
Soledad <figueroasoledad89@gmail.com>
2023-01-21 18:08:17 -0300

    first commit

---
+++ clase2/main.js
@@ -0,0 +1,6 @@
+// var btnMenu = document.querySelector('button.iconMenu');
+// var navPrincipal = document.querySelector('header.principal nav');
+
+// btnMenu.onclick = function(){
+//     navPrincipal.classList.toggle('active')
+// }
---
+++ proyectoWeb/js/main.js
@@ -0,0 +1,43 @@
+var btnMenu = document.querySelector('button.iconMenu');
+var navPrincipal = document.querySelector('header.principal nav');
+
+btnMenu.onclick = function(){ 
+navPrincipal.classList.toggle('active')
+btnMenu.classList.toggle('active')
+}
+
+var fondos = document.querySelectorAll(".imagen");
+
+var flechaPrev = document.querySelector(".flecha .prev");
+flechaPrev.onclick = function (){
+
+	for(var i = 0 ; i < fondos.length ; i++){
+		if(fondos[i].classList.contains("active")){
+			fondos[i].classList.toggle("active")
+
+			if(i-1 == -1){
+				fondos[fondos.length-1].classList.toggle("active")
+			}else{
+				fondos[i-1].classList.toggle("active")
+			}
+			break;
+		}
+	}
+}
+
+var flechaNext = document.querySelector(".flecha .next");
+flechaNext.onclick = function (){
+	
+	for(var i = 0 ; i < fondos.length ; i++){
+		if(fondos[i].classList.contains("active")){
+			fondos[i].classList.toggle("active")
+
+			if(i+1 == fondos.length){
+				fondos[0].classList.toggle("active")
+			}else{
+				fondos[i+1].classList.toggle("active")
+			}
+			break;
+		}
+	}
+}
